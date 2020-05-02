8b80618e1541b52eb484b283d70bf991d4d1db0a
leventsov-i <leventsov-i@yandex.ru>
2020-05-03 02:20:42 +0300

    delete unused methods

--- src/main/java/ru/auth/service/UserService.java
+++ src/main/java/ru/auth/service/UserService.java
@@ -55,17 +55,4 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
         return true;
     }
-
-    public boolean deleteUser(Long userId) {
-        if (userRepository.findById(userId).isPresent()) {
-            userRepository.deleteById(userId);
-            return true;
-        }
-        return false;
-    }
-
-    public List<User> usergtList(Long idMin) {
-        return em.createQuery("SELECT u FROM User u WHERE u.id > :paramId", User.class)
-                .setParameter("paramId", idMin).getResultList();
-    }
 }
